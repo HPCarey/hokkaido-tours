00142961447fcbadabde5c56574a95e472295701
Hannah Carey <hannahpcarey@gmail.com>
2023-12-22 21:39:32 +0000

    add shopping cart logic to add items and price

--- hokkaido-tours/assets/scripts/shoppingcart.js
+++ hokkaido-tours/assets/scripts/shoppingcart.js
@@ -1,103 +1,44 @@
-let productsInCart = JSON.parse(localStorage.getItem('shoppingCart'));
-if(!productsInCart){
-	productsInCart = [];
-}
-const parentElement = document.querySelector('#buyItems');
-const cartSumPrice = document.querySelector('#sum-prices');
-const products = document.querySelectorAll('.product-under');
+document.addEventListener('DOMContentLoaded', function () {
+    // Define an array to store items in the shopping cart
+    const shoppingCartItems = [];
 
+    // Function to update the shopping cart content
+    function updateShoppingCart() {
+        const cartBody = document.querySelector('.offcanvas-body');
+        // Clear the existing content
+        cartBody.innerHTML = '';
 
-const countTheSumPrice = function () { // 4
-	let sum = 0;
-	productsInCart.forEach(item => {
-		sum += item.price;
-	});
-	return sum;
-}
+        // Calculate the total sum
+        let totalSum = 0;
 
-const updateShoppingCartHTML = function () {  // 3
-	localStorage.setItem('shoppingCart', JSON.stringify(productsInCart));
-	if (productsInCart.length > 0) {
-		let result = productsInCart.map(product => {
-			return `
-				<li class="buyItem">
-					<img src="${product.image}">
-					<div>
-						<h5>${product.name}</h5>
-						<h6>$${product.price}</h6>
-						<div>
-							<button class="button-minus" data-id=${product.id}>-</button>
-							<span class="countOfProduct">${product.count}</span>
-							<button class="button-plus" data-id=${product.id}>+</button>
-						</div>
-					</div>
-				</li>`
-		});
-		parentElement.innerHTML = result.join('');
-		document.querySelector('.checkout').classList.remove('hidden');
-		cartSumPrice.innerHTML = '$' + countTheSumPrice();
+        // Iterate through items and add them to the cart
+        shoppingCartItems.forEach(item => {
+            const itemElement = document.createElement('div');
+            itemElement.innerHTML = `<p>${item.name} - $${item.price.toFixed(2)}</p>`;
+            cartBody.appendChild(itemElement);
 
-	}
-	else {
-		document.querySelector('.checkout').classList.add('hidden');
-		parentElement.innerHTML = '<h4 class="empty">Your shopping cart is empty</h4>';
-		cartSumPrice.innerHTML = '';
-	}
-}
+            // Add the price to the total sum
+            totalSum += item.price;
+        });
 
-function updateProductsInCart(product) { // 2
-	for (let i = 0; i < productsInCart.length; i++) {
-		if (productsInCart[i].id == product.id) {
-			productsInCart[i].count += 1;
-			productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
-			return;
-		}
-	}
-	productsInCart.push(product);
-}
+        // Display the total sum
+        const totalSumElement = document.getElementById('sum-prices');
+        totalSumElement.textContent = `Total: $${totalSum.toFixed(2)}`;
+    }
 
-products.forEach(item => {   // 1
-	item.addEventListener('click', (e) => {
-		if (e.target.classList.contains('addToCart')) {
-			const productID = e.target.dataset.productId;
-			const productName = item.querySelector('.productName').innerHTML;
-			const productPrice = item.querySelector('.priceValue').innerHTML;
-			const productImage = item.querySelector('img').src;
-			let product = {
-				name: productName,
-				image: productImage,
-				id: productID,
-				count: 1,
-				price: +productPrice,
-				basePrice: +productPrice,
-			}
-			updateProductsInCart(product);
-			updateShoppingCartHTML();
-		}
-	});
+    // Attach click event listeners to the "Purchase Tour" buttons
+    const purchaseButtons = document.querySelectorAll('.btn-purchase-tour');
+    purchaseButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const card = this.closest('.card');
+            const tourName = card.querySelector('.card-title').textContent;
+            const tourPrice = parseFloat(card.querySelector('.priceValue').textContent);
+
+            // Add the item to the shopping cart array
+            shoppingCartItems.push({ name: tourName, price: tourPrice });
+
+            // Update the shopping cart content
+            updateShoppingCart();
+        });
+    });
 });
-
-parentElement.addEventListener('click', (e) => { // Last
-	const isPlusButton = e.target.classList.contains('button-plus');
-	const isMinusButton = e.target.classList.contains('button-minus');
-	if (isPlusButton || isMinusButton) {
-		for (let i = 0; i < productsInCart.length; i++) {
-			if (productsInCart[i].id == e.target.dataset.id) {
-				if (isPlusButton) {
-					productsInCart[i].count += 1
-				}
-				else if (isMinusButton) {
-					productsInCart[i].count -= 1
-				}
-				productsInCart[i].price = productsInCart[i].basePrice * productsInCart[i].count;
-
-			}
-			if (productsInCart[i].count <= 0) {
-				productsInCart.splice(i, 1);
-			}
-		}
-		updateShoppingCartHTML();
-	}
-});
-
-updateShoppingCartHTML();
